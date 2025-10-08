4f21bda7273494c5706e1e919d2663b69963a750
egipsinargo <egipsinargo123@gmail.com>
2025-10-08 14:47:54 +0700

    add spotify api connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:music_player/app.dart';
 import 'data/services/firebase_options.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env');
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
